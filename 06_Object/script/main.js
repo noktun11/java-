cb999d0e64cd320007ee91be34b59d8311b9bc6e
noktun11 <noktun@naver.com>
2024-02-01 09:28:03 +0900

    20240201

---
+++ 06_Object/script/main.js
@@ -0,0 +1,13 @@
+//우리 며칠째 버튼 클릭시 d-day계산및 결과 표시
+//Form : v폼
+
+//1. DOM 탐색
+const dday_form = document.querySelector("dday_form");
+
+//2. DOM 
+dday_form.addEventListener("submit", showDday);
+
+//3. 이벤트 처리기(함수)
+function showDday(){
+
+}
